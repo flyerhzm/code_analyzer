a43cddaeb7acb9c5b459369615d325927134a006
Benjamin Fleischer <github@benjaminfleischer.com>
2013-08-13 09:01:32 -0500

    Rename :line to :line_number so as not to over-write the :line method
    Over-writing :line breaks ruby_parser or any other libraries that
    do not expect the monkey-patch

--- lib/code_analyzer/sexp.rb
+++ lib/code_analyzer/sexp.rb
@@ -13,22 +13,22 @@ class Sexp
   #
   #     s(:@ident, "test", s(2, 12)
   #       => 2
-  def line
+  def line_number
     case sexp_type
     when :def, :defs, :command, :command_call, :call, :fcall, :method_add_arg, :method_add_block,
          :var_ref, :vcall, :const_ref, :const_path_ref, :class, :module,
          :if, :unless, :elsif, :ifop, :if_mod, :unless_mod, :binary,
          :alias, :symbol_literal, :symbol, :aref, :hash, :assoc_new, :string_literal,
          :massign
-      self[1].line
+      self[1].line_number
     when :assoclist_from_args, :bare_assoc_hash
-      self[1][0].line
+      self[1][0].line_number
     when :string_add, :opassign
-      self[2].line
+      self[2].line_number
     when :array
-      array_values.first.line
+      array_values.first.line_number
     when :mlhs_add
-      self.last.line
+      self.last.line_number
     else
       self.last.first if self.last.is_a? Array
     end
